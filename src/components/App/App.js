7c1b708c797dff239a91e6faa5c83231dbac37a3
Vrezh <hovanisyan.vrezh@gmail.com>
2018-08-05 21:50:31 +0400

    [fix] grammar

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -7,7 +7,7 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <h1 className="App-title">Welcome to Solaris Project</h1>
+          <h1 className="App-title">Welcome to Polaris Project</h1>
         </header>
       </div>
     );
