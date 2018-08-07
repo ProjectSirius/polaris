e75e0116dac503aa3fc3fed3ae3048157dd168e6
Angela <ang.hayrapetian@gmail.com>
2018-08-07 15:03:15 +0400

    [add] passing signUp action as a prop to SignUpAudience

--- src/containers/SignUpAudienceContainer.js
+++ src/containers/SignUpAudienceContainer.js
@@ -1,7 +1,13 @@
 import { connect } from 'react-redux';
 import { reduxForm } from 'redux-form';
 
+import signUp from '../actions/signUp';
+
 import SignUpAudience from '../components/SignUpAudience';
+
+const mapDispatchToProps = dispatch => ({
+  signUp: user => dispatch(signUp(user)),
+});
 
 const SignUpAudienceForm = reduxForm({
   form: 'SignUpAudienceForm',
@@ -9,5 +15,5 @@ const SignUpAudienceForm = reduxForm({
 
 export default connect(
   null,
-  null
+  mapDispatchToProps
 )(SignUpAudienceForm);
