2af60ed1f46c85667a2d64007bbdbdfd8db5cd43
Vrezh <hovanisyan.vrezh@gmail.com>
2018-08-07 16:37:56 +0400

    [add] routing for non-protected routes

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,4 +1,10 @@
 import React, { Component } from 'react';
+import { Route, Switch } from 'react-router-dom';
+
+import HomePage from '../HomePage';
+import LogIn from '../LogIn';
+import SignUpAudience from '../SignUpAudience';
+import SignUpContentMaker from '../SignUpContentMaker';
 
 import './App.css';
 
@@ -9,6 +15,13 @@ class App extends Component {
         <header className="App-header">
           <h1 className="App-title">Welcome to Polaris Project</h1>
         </header>
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route path="/login" component={LogIn} />
+          <Route path="/signUpAudience" component={SignUpAudience} />
+          <Route path="/signUpContentMaker" component={SignUpContentMaker} />
+          <Route render={() => <h1>Not Found App</h1>} />
+        </Switch>
       </div>
     );
   }
