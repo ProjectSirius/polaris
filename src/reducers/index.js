2826c563d25f66e7ad779222529d28a55e36cf1e
Angela <ang.hayrapetian@gmail.com>
2018-08-07 11:57:24 +0400

    [add] redux form reducer to combineReducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,1 +1,6 @@
-export default (state = {}, action) => state;
+import { combineReducers } from 'redux';
+import { reducer as formReducer } from 'redux-form';
+
+export default combineReducers({
+  form: formReducer,
+});
