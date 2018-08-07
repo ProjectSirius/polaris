4523da88e917a51e5f72503f2637f51be26eff94
Vrezh <hovanisyan.vrezh@gmail.com>
2018-08-08 00:13:55 +0400

    [add] change lang reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,8 +2,10 @@ import { combineReducers } from 'redux';
 import { reducer as formReducer } from 'redux-form';
 
 import isSignUp from './isSignUp';
+import lang from './lang';
 
 export default combineReducers({
   form: formReducer,
   isSignUp,
+  lang,
 });
