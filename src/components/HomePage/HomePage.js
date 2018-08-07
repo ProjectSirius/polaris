5ff41b231d811566d6103e8f2485ff184684770a
Vrezh <hovanisyan.vrezh@gmail.com>
2018-08-07 22:29:05 +0400

    [upd] make home-page stateless

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -1,21 +1,9 @@
 import React from 'react';
-import { injectIntl, defineMessages } from 'react-intl';
 
 import './styles.css';
 
-const messages = defineMessages({
-  title: {
-    id: 'home-page-title',
-    defaultMessage: 'Home Page',
-  },
-});
-
-const HomePage = props => {
-  const {
-    intl: { formatMessage },
-  } = props;
-
-  return <div>{formatMessage(messages.title)}</div>;
+const HomePage = ({ title }) => {
+  return <div>{title}</div>;
 };
 
-export default injectIntl(HomePage);
+export default HomePage;
