0ac60795700163ae294484a1375f2081291b223e
Vrezh <hovanisyan.vrezh@gmail.com>
2018-08-07 23:09:22 +0400

    [upd] make root stateless

--- src/components/Root/Root.js
+++ src/components/Root/Root.js
@@ -1,27 +1,14 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import { PropTypes } from 'prop-types';
-import queryString from 'query-string';
 import { BrowserRouter as Router } from 'react-router-dom';
-import { IntlProvider, addLocaleData } from 'react-intl';
-import ruLocaleData from 'react-intl/locale-data/ru';
+import { IntlProvider } from 'react-intl';
 
-import translations from '../../i18n/locales';
 import AppContainer from '../../containers/AppContainer';
 
 import './styles.css';
 
-addLocaleData(ruLocaleData);
-
-const Root = ({ store }) => {
-  /*
-   * it's plain object
-   * if url = smth.com/search=1&locale=en then it looks like {search: 1, locale: 'en'}
-   */
-  const parseUrl = queryString.parse(window.location.search);
-  const locale = parseUrl.locale || 'en';
-  const messages = translations[locale];
-
+const Root = ({ store, locale, messages }) => {
   return (
     <Provider store={store}>
       <Router>
