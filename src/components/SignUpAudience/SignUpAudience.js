ea53c7215d1b5842b3e92381390dcb62c9d4e7d6
Angela <ang.hayrapetian@gmail.com>
2018-08-07 22:26:25 +0400

    [add] redirect from audience_owner sign up page to login page

--- src/components/SignUpAudience/SignUpAudience.js
+++ src/components/SignUpAudience/SignUpAudience.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import { Field } from 'redux-form';
+import { Redirect } from 'react-router-dom';
 
 import renderField from './RenderField';
 
 import './styles.css';
 
-const SignUpAudience = ({ signUp, handleSubmit }) => {
-  return (
+const SignUpAudience = ({ signUp, handleSubmit, isSignUp, location }) => {
+  const { from } = location.state || { from: { pathname: '/login' } };
+
+  return isSignUp ? (
+    <Redirect to={from} />
+  ) : (
     <form onSubmit={handleSubmit(signUp)}>
       <h1>Audience Owner Sign Up</h1>
       <Field
