a0f637c970251b5604c8c6972bcf8241d7b253d1
Angela <ang.hayrapetian@gmail.com>
2018-08-07 16:57:38 +0400

    [change] signUpProcess to fakeFetch

--- src/actions/signUp.js
+++ src/actions/signUp.js
@@ -1,28 +1,36 @@
 import { SIGN_UP_FAILURE, SIGN_UP_REQUEST, SIGN_UP_SUCCESS } from './constants';
 
-const signUpProcess = (user, error) => {
-  setTimeout(() => {
-    if (user) {
-      return error(null);
+const fakeFetch = userData => {
+  return new Promise((resolve, reject) => {
+    const rand = Math.random();
+
+    if (rand > 0.2) {
+      setTimeout(() => {
+        return resolve(true);
+      }, 1000);
     } else {
-      return error(new Error('Sign Up Failed'));
+      setTimeout(() => {
+        return reject(false);
+      }, 2000);
     }
-  }, 1000);
+  });
 };
 
-const signUpRequest = user => ({ type: SIGN_UP_REQUEST, payload: user });
-const signUpSuccess = user => ({ type: SIGN_UP_SUCCESS, payload: user });
+const signUpRequest = () => ({ type: SIGN_UP_REQUEST });
+const signUpSuccess = user => ({
+  type: SIGN_UP_SUCCESS,
+  payload: {
+    user,
+  },
+});
 const signUpFailure = error => ({ type: SIGN_UP_FAILURE, error });
 
 const signUp = user => dispatch => {
-  signUpProcess(user, error => {
-    dispatch(signUpRequest(user));
-    if (!error) {
-      dispatch(signUpSuccess(user));
-    } else {
-      dispatch(signUpFailure(error));
-    }
-  });
+  dispatch(signUpRequest(user));
+
+  fakeFetch(user)
+    .then(() => dispatch(signUpSuccess(user)))
+    .catch(msg => dispatch(signUpFailure(msg)));
 };
 
 export default signUp;
