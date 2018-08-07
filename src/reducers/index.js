135cff72dced7a72c056d54405dcb6095b3dab21
Angela <ang.hayrapetian@gmail.com>
2018-08-07 22:23:03 +0400

    [add] isSignUp reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,9 @@
 import { combineReducers } from 'redux';
 import { reducer as formReducer } from 'redux-form';
 
+import isSignUp from './isSignUp';
+
 export default combineReducers({
   form: formReducer,
+  isSignUp,
 });
