4401f8f235b84fbb2667675b42753f45dd53a37e
Vrezh <hovanisyan.vrezh@gmail.com>
2018-08-07 21:23:21 +0400

    [add] react-intl provider

--- src/components/Root/Root.js
+++ src/components/Root/Root.js
@@ -2,16 +2,28 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import { PropTypes } from 'prop-types';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { IntlProvider, addLocaleData } from 'react-intl';
+import arLocaleData from 'react-intl/locale-data/ar';
+import esLocaleData from 'react-intl/locale-data/es';
 
+import translations from '../../i18n/locales';
 import App from '../App';
 
 import './styles.css';
 
+addLocaleData(arLocaleData);
+addLocaleData(esLocaleData);
+
 const Root = ({ store }) => {
+  const locale = window.location.search.replace('?locale=', '') || 'en';
+  const messages = translations[locale];
+
   return (
     <Provider store={store}>
       <Router>
-        <App />
+        <IntlProvider locale={locale} key={locale} messages={messages}>
+          <App />
+        </IntlProvider>
       </Router>
     </Provider>
   );
