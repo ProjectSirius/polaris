b7d94c570b92aa7639ce93a46095df794a6d3d4b
Vrezh <hovanisyan.vrezh@gmail.com>
2018-08-09 02:36:19 +0400

    [add] isMenuOpen param and menu toggle fn

--- src/containers/MainNavBarContainer.js
+++ src/containers/MainNavBarContainer.js
@@ -2,16 +2,27 @@ import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 
 import MainNavBar from '../components/MainNavBar';
+import { toggleMenu } from '../actions/index';
 
-import { selectIsAuth, selectCurrentUser, selectLanguage } from '../selectors';
+import {
+  selectIsAuth,
+  selectCurrentUser,
+  selectLanguage,
+  selectIsOpenMainMenu,
+} from '../selectors';
 
 const mapStateToProps = createStructuredSelector({
   isAuth: selectIsAuth,
   currentUser: selectCurrentUser,
   lang: selectLanguage,
+  isOpen: selectIsOpenMainMenu,
+});
+
+const mapDispatchToProps = dispatch => ({
+  toggleMenu: isOpen => dispatch(toggleMenu(isOpen)),
 });
 
 export default connect(
   mapStateToProps,
-  null
+  mapDispatchToProps
 )(MainNavBar);
