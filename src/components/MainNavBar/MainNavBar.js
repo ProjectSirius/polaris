1a02889627c087459e4304d46eea14f115f41e26
Vrezh <hovanisyan.vrezh@gmail.com>
2018-08-09 02:42:24 +0400

    [upd] Nav Bar to class component

--- src/components/MainNavBar/MainNavBar.js
+++ src/components/MainNavBar/MainNavBar.js
@@ -4,112 +4,126 @@ import { Link } from 'react-router-dom';
 
 import './styles.css';
 
-const MainNavBar = props => {
-  const path = window.location.pathname;
-  const { lang, isOpen, toggleMenu } = props;
+class MainNavBar extends React.Component {
+  constructor() {
+    super();
 
-  return (
-    <header className="main-nav-header">
-      <div className="top-nav">
-        <div className="hamburger" onClick={() => toggleMenu(!isOpen)}>
-          <Glyphicon glyph="glyphicon glyphicon-align-justify" />
-          <span className="hamburger-title">MENU</span>
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    const { toggleMenu, isOpen } = this.props;
+
+    toggleMenu(!isOpen);
+  }
+
+  render() {
+    const path = window.location.pathname;
+    const { lang, isOpen } = this.props;
+
+    return (
+      <header className="main-nav-header">
+        <div className="top-nav">
+          <div className="hamburger" onClick={this.handleClick}>
+            <Glyphicon glyph="glyphicon glyphicon-align-justify" />
+            <span className="hamburger-title">MENU</span>
+          </div>
+          <div className="main-nav-title-wrapper">
+            <h2 className="main-nav-title">
+              <Link
+                to={{
+                  pathname: '/',
+                  search: `?locale=${lang}`,
+                }}
+              >
+                Polaris
+              </Link>
+            </h2>
+          </div>
+          <div className="languages-wrapper">
+            <div className="languages">
+              <div>
+                <DropdownButton
+                  bsStyle="default"
+                  title={lang}
+                  id="lang-dropdown"
+                  pullRight
+                >
+                  <MenuItem
+                    href={`${path}?locale=ru`}
+                    eventKey="1"
+                    active={lang === 'ru' ? true : false}
+                  >
+                    Русский
+                  </MenuItem>
+                  <MenuItem divider />
+                  <MenuItem
+                    href={`${path}?locale=en`}
+                    eventKey="2"
+                    active={lang === 'en' ? true : false}
+                  >
+                    English
+                  </MenuItem>
+                </DropdownButton>
+              </div>
+            </div>
+          </div>
         </div>
-        <div className="main-nav-title-wrapper">
-          <h2 className="main-nav-title">
-            <Link
-              to={{
-                pathname: '/',
-                search: `?locale=${lang}`,
-              }}
-            >
-              Polaris
-            </Link>
-          </h2>
-        </div>
-        <div className="languages-wrapper">
-          <div className="languages">
-            <div>
+        <div className={isOpen ? 'main-nav main-nav-opened' : 'main-nav'}>
+          <nav className="nav-links">
+            <div className="nav-link">
+              <Link
+                to={{
+                  pathname: '/audience',
+                  search: `?locale=${lang}`,
+                }}
+              >
+                Audience
+              </Link>
+            </div>
+            <div className="nav-link">
+              <Link
+                to={{
+                  pathname: '/contentowner',
+                  search: `?locale=${lang}`,
+                }}
+              >
+                Content Owner
+              </Link>
+            </div>
+          </nav>
+          <div className="nav-links-auth">
+            <div className="nav-link nav-link-login">
+              <Link
+                to={{
+                  pathname: '/login',
+                  search: `?locale=${lang}`,
+                }}
+              >
+                Login
+              </Link>
+            </div>
+            <div className="dropdown-btn">
               <DropdownButton
-                bsStyle="default"
-                title={lang}
-                id="lang-dropdown"
+                id="dropdown-sign-up"
+                title="SIGN UP"
+                bsStyle="info"
                 pullRight
               >
-                <MenuItem
-                  href={`${path}?locale=ru`}
-                  eventKey="1"
-                  active={lang === 'ru' ? true : false}
-                >
-                  Русский
+                {' '}
+                <MenuItem href={`/signUpAudience?locale=ru`} eventKey="1">
+                  SignUp for Audience
                 </MenuItem>
-                <MenuItem divider />
-                <MenuItem
-                  href={`${path}?locale=en`}
-                  eventKey="2"
-                  active={lang === 'en' ? true : false}
-                >
-                  English
+                <MenuItem href={`/signUpContentMaker?locale=ru`} eventKey="1">
+                  SignUp for Content Owner
                 </MenuItem>
               </DropdownButton>
             </div>
           </div>
         </div>
-      </div>
-      <div className={isOpen ? 'main-nav main-nav-opened' : 'main-nav'}>
-        <nav className="nav-links">
-          <div className="nav-link">
-            <Link
-              to={{
-                pathname: '/audience',
-                search: `?locale=${lang}`,
-              }}
-            >
-              Audience
-            </Link>
-          </div>
-          <div className="nav-link">
-            <Link
-              to={{
-                pathname: '/contentowner',
-                search: `?locale=${lang}`,
-              }}
-            >
-              Content Owner
-            </Link>
-          </div>
-        </nav>
-        <div className="nav-links-auth">
-          <div className="nav-link nav-link-login">
-            <Link
-              to={{
-                pathname: '/login',
-                search: `?locale=${lang}`,
-              }}
-            >
-              Login
-            </Link>
-          </div>
-          <div className="dropdown-btn">
-            <DropdownButton
-              id="dropdown-sign-up"
-              title="SIGN UP"
-              bsStyle="info"
-              pullRight
-            >
-              {' '}
-              <MenuItem href={`/signUpAudience?locale=ru`} eventKey="1">
-                SignUp for Audience
-              </MenuItem>
-              <MenuItem href={`/signUpContentMaker?locale=ru`} eventKey="1">
-                SignUp for Content Owner
-              </MenuItem>
-            </DropdownButton>
-          </div>
-        </div>
-      </div>
-    </header>
-  );
-};
+      </header>
+    );
+  }
+}
 
 export default MainNavBar;
