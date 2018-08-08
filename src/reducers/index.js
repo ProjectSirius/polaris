3e5101e88a9c2b9f16975724f47c188a4b5c7edb
Angela <ang.hayrapetian@gmail.com>
2018-08-08 13:03:06 +0400

    [add] current user to root reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,9 +3,11 @@ import { reducer as formReducer } from 'redux-form';
 
 import isSignUp from './isSignUp';
 import lang from './lang';
+import currentUser from './currentUser';
 
 export default combineReducers({
   form: formReducer,
   isSignUp,
   lang,
+  currentUser,
 });
