3511c42dc5dd9a1c8249f172deaf46685748d729
Angela <ang.hayrapetian@gmail.com>
2018-08-08 23:42:29 +0400

    [add] bootstrap classes to sign up forms

--- src/components/SignUpContentMaker/SignUpContentMaker.js
+++ src/components/SignUpContentMaker/SignUpContentMaker.js
@@ -1,43 +1,72 @@
 import React from 'react';
 import { Field } from 'redux-form';
 import { Redirect } from 'react-router-dom';
+import { Button } from 'react-bootstrap';
 
 import renderField from './RenderField';
 
 import './styles.css';
 
-const SignUpContentMaker = ({ signUp, handleSubmit, isSignUp, location }) => {
+const SignUpContentMaker = ({
+  signUp,
+  handleSubmit,
+  isSignUp,
+  location,
+  valid,
+}) => {
   const { from } = location.state || { from: { pathname: '/login' } };
 
   return isSignUp ? (
     <Redirect to={from} />
   ) : (
-    <form onSubmit={handleSubmit(signUp)}>
-      <h1>Sign Up Content Maker</h1>
-      <Field
-        name="username"
-        type="text"
-        component={renderField}
-        label="Username"
-      />
-      <Field name="email" type="email" component={renderField} label="Email" />
-      <Field
-        name="password"
-        type="password"
-        component={renderField}
-        label="Password"
-      />
-      <Field
-        name="passwordConformation"
-        type="password"
-        component={renderField}
-        label="Password Conformation"
-      />
-      <Field name="userType" type="hidden" component={renderField} />
-      <div>
-        <button type="submit">Submit</button>
-      </div>
-    </form>
+    <div className="sign-up-form">
+      <form onSubmit={handleSubmit(signUp)}>
+        <h1>Sign Up Content Maker</h1>
+        <Field
+          name="username"
+          type="text"
+          component={renderField}
+          placeholder="Username"
+          bsSize="large"
+          className="input"
+        />
+        <Field
+          name="email"
+          type="email"
+          component={renderField}
+          placeholder="Email"
+          bsSize="large"
+          className="input"
+        />
+        <Field
+          name="password"
+          type="password"
+          component={renderField}
+          placeholder="Password"
+          bsSize="large"
+          className="input"
+        />
+        <Field
+          name="passwordConformation"
+          type="password"
+          component={renderField}
+          placeholder="Password Conformation"
+          bsSize="large"
+          className="input"
+        />
+        <Field
+          name="userType"
+          type="hidden"
+          component={renderField}
+          className="hidden-input"
+        />
+        <div>
+          <Button bsStyle="primary" block disabled={!valid} bsSize="large">
+            Submit
+          </Button>
+        </div>
+      </form>
+    </div>
   );
 };
 
