b6eeb826ff2af5567be73bfac7dd87c206381c04
Vrezh <hovanisyan.vrezh@gmail.com>
2018-08-09 02:34:51 +0400

    [add] toggle menu action

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,6 @@
 import signUp from './signUp';
 import changeLang from './lang';
 import login from './login';
+import toggleMenu from './main-menu';
 
-export { signUp, changeLang, login };
+export { signUp, changeLang, login, toggleMenu };
