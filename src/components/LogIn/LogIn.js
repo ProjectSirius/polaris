a01f88c98a0b4545f61df83baf4702d9b6e6da01
Angela <ang.hayrapetian@gmail.com>
2018-08-08 13:06:05 +0400

    [add] login form

--- src/components/LogIn/LogIn.js
+++ src/components/LogIn/LogIn.js
@@ -1,9 +1,35 @@
 import React from 'react';
+import { Field } from 'redux-form';
+import { Button } from 'react-bootstrap';
+
+import RenderField from './RenderField';
 
 import './styles.css';
 
-const LogIn = props => {
-  return <div>Log In</div>;
+const LogIn = ({ handleSubmit, login }) => {
+  return (
+    <form onSubmit={handleSubmit(login)} className="login-form">
+      <Field
+        name="username"
+        component={RenderField}
+        type="text"
+        label="Text"
+        bsSize="large"
+        placeholder="Your Username"
+      />
+      <Field
+        name="password"
+        component={RenderField}
+        type="password"
+        label="Password"
+        placeholder="Your Password"
+        bsSize="large"
+      />
+      <Button bsStyle="primary" bsSize="large" block type="submit">
+        Log In
+      </Button>
+    </form>
+  );
 };
 
 export default LogIn;
