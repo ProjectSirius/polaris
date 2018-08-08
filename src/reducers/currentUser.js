37fb4131b0fca42c046b8d952a95b64f357286cf
Angela <ang.hayrapetian@gmail.com>
2018-08-08 13:01:30 +0400

    [add] currentUser reducer

---
+++ src/reducers/currentUser.js
@@ -0,0 +1,19 @@
+import {
+  LOGIN_SUCCESS,
+  LOGIN_FAILURE,
+  LOGOUT_SUCCESS,
+} from '../actions/constants';
+
+const currentUser = (state = null, action) => {
+  switch (action.type) {
+    case LOGIN_SUCCESS:
+      return action.payload;
+    case LOGIN_FAILURE:
+    case LOGOUT_SUCCESS:
+      return null;
+    default:
+      return state;
+  }
+};
+
+export default currentUser;
