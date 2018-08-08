a0dbb3bcb902ce559f3dae83751bbf0b8a8932a8
Angela <ang.hayrapetian@gmail.com>
2018-08-08 14:19:14 +0400

    [add] isAuth to root reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -4,10 +4,12 @@ import { reducer as formReducer } from 'redux-form';
 import isSignUp from './isSignUp';
 import lang from './lang';
 import currentUser from './currentUser';
+import isAuth from './isAuth';
 
 export default combineReducers({
   form: formReducer,
   isSignUp,
   lang,
   currentUser,
+  isAuth,
 });
