1f347755ee4cb372aa22716c82623abf480d0320
Vrezh <hovanisyan.vrezh@gmail.com>
2018-08-09 03:18:59 +0400

    [add] logOut action creator

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,8 @@
 import signUp from './signUp';
 import changeLang from './lang';
 import login from './login';
+import logOut from './logOut';
+
 import toggleMenu from './main-menu';
 
-export { signUp, changeLang, login, toggleMenu };
+export { signUp, changeLang, login, toggleMenu, logOut };
