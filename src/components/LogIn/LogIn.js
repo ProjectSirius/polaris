1babf0b653c9d7be9fc03b96f9fd67fe0b67fa0f
Angela <ang.hayrapetian@gmail.com>
2018-08-08 14:19:55 +0400

    [add] redirect after successfully logging in

--- src/components/LogIn/LogIn.js
+++ src/components/LogIn/LogIn.js
@@ -1,13 +1,23 @@
 import React from 'react';
 import { Field } from 'redux-form';
+import { Redirect } from 'react-router-dom';
 import { Button } from 'react-bootstrap';
 
 import RenderField from './RenderField';
 
 import './styles.css';
 
-const LogIn = ({ handleSubmit, login }) => {
-  return (
+const LogIn = ({ handleSubmit, login, isAuth, location, currentUser }) => {
+  const redirectPath = isAuth
+    ? currentUser.userType === 'audience_owner'
+      ? 'audience'
+      : 'contentowner'
+    : '';
+  const { from } = location.state || { from: { pathname: `/${redirectPath}` } };
+
+  return isAuth ? (
+    <Redirect to={from} />
+  ) : (
     <form onSubmit={handleSubmit(login)} className="login-form">
       <Field
         name="username"
