0ca773696d2628356e498fc7247b3949f3b4c64e
Angela <ang.hayrapetian@gmail.com>
2018-08-09 13:13:59 +0400

    [reformat] with prettier

--- src/components/ContentOwnerHomePage/ContentOwnerHomePage.js
+++ src/components/ContentOwnerHomePage/ContentOwnerHomePage.js
@@ -1,10 +1,14 @@
 import React from 'react';
-import ViewBtnGroup from '../View-btn-group'
+import ViewBtnGroup from '../View-btn-group';
 
 import './styles.css';
 
-const ContentOwnerHomePage = props => {
-  return <div>Content Owner Home Page <ViewBtnGroup/></div>;
+const ContentOwnerHomePage = () => {
+  return (
+    <div>
+      Content Owner Home Page <ViewBtnGroup />
+    </div>
+  );
 };
 
 export default ContentOwnerHomePage;
--- src/components/View-btn-group/btn-group.js
+++ src/components/View-btn-group/btn-group.js
@@ -1,20 +1,19 @@
 import React, { Component } from 'react';
-import { Button, ButtonGroup, Glyphicon  } from 'react-bootstrap';
-
+import { Button, ButtonGroup, Glyphicon } from 'react-bootstrap';
 
 class btnGroup extends Component {
   render() {
-	return (
-	  <ButtonGroup>
-		<Button>
-			<Glyphicon glyph="th-large" />
-		</Button>
-		<Button>
-			<Glyphicon glyph="th-list" />
-		</Button>
-		<Button>Map</Button>
-	  </ButtonGroup>
-	);
+    return (
+      <ButtonGroup>
+        <Button>
+          <Glyphicon glyph="th-large" />
+        </Button>
+        <Button>
+          <Glyphicon glyph="th-list" />
+        </Button>
+        <Button>Map</Button>
+      </ButtonGroup>
+    );
   }
 }
 
