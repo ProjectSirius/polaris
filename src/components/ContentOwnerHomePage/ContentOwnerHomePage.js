e4ece376d2903059bba13ba2dc6388c97368997e
Vahan Sargsyan <vahansargsyan1310@gmail.com>
2018-08-09 14:16:25 +0400

    [add] Filter bar container div and genres.json.

--- src/components/ContentOwnerHomePage/ContentOwnerHomePage.js
+++ src/components/ContentOwnerHomePage/ContentOwnerHomePage.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import ViewBtnGroup from '../View-btn-group'
+import FilterBar from '../filter-bar';
 
 import './styles.css';
 
 const ContentOwnerHomePage = props => {
-  return <div>Content Owner Home Page <ViewBtnGroup/></div>;
+  return (
+    <div>
+      <p>Content Owner Home Page</p> 
+      <FilterBar />
+      <ViewBtnGroup/>
+    </div>
+  );
 };
 
 export default ContentOwnerHomePage;
