205075a281bbc0a6968dc402b918edfe1c9bceee
Angela <ang.hayrapetian@gmail.com>
2018-08-09 13:34:08 +0400

    [add] 'Passwords do not match' error message

--- src/helpers/signUpValidate.js
+++ src/helpers/signUpValidate.js
@@ -20,6 +20,10 @@ const signUpValidate = values => {
       errors[field] = 'Must be a valid email';
     }
 
+    if (field === 'passwordConformation' && field !== requiredFields[2]) {
+      errors[field] = 'Passwords do not match!';
+    }
+
     if (values[field] && values[field].length < 5) {
       errors[field] = 'Your input must contain more than 5 symbols!';
     }
