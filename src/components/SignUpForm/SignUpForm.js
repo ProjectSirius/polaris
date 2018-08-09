28df01cb65c21d1a03802528b3eab276baafa409
Vrezh <hovanisyan.vrezh@gmail.com>
2018-08-09 22:09:10 +0400

    [add] sign up component

---
+++ src/components/SignUpForm/SignUpForm.js
@@ -0,0 +1,78 @@
+import React from 'react';
+import { Field } from 'redux-form';
+import { Redirect } from 'react-router-dom';
+import { Alert } from 'react-bootstrap';
+
+import renderField from './RenderField';
+import SubmitBtn from '../SubmitBtn';
+
+import './styles.css';
+
+const SignUpForm = ({
+  signUp,
+  handleSubmit,
+  isSignUp,
+  location,
+  valid,
+  isSignUpRequesting,
+  errorSignUp,
+  formTitle,
+}) => {
+  const { from } = location.state || { from: { pathname: '/login' } };
+  return isSignUp ? (
+    <Redirect to={from} />
+  ) : (
+    <div className="sign-up-form">
+      <form onSubmit={handleSubmit(signUp)}>
+        <h1>{formTitle}</h1>
+        <Field
+          name="username"
+          type="text"
+          component={renderField}
+          placeholder="Username"
+          bsSize="large"
+          className="input"
+        />
+        <Field
+          name="email"
+          type="email"
+          component={renderField}
+          placeholder="Email"
+          bsSize="large"
+          className="input"
+        />
+        <Field
+          name="password"
+          type="password"
+          component={renderField}
+          placeholder="Password"
+          bsSize="large"
+          className="input"
+        />
+        <Field
+          name="passwordConformation"
+          type="password"
+          component={renderField}
+          placeholder="Password Conformation"
+          bsSize="large"
+          className="input"
+        />
+        <Field
+          name="userType"
+          type="hidden"
+          component={renderField}
+          className="hidden-input"
+        />
+        {isSignUpRequesting && <Alert bsStyle="info">Please wait...</Alert>}
+        {errorSignUp && <Alert bsStyle="danger">{errorSignUp}</Alert>}
+        <SubmitBtn
+          value="CREATE ACCOUNT!"
+          valid={valid}
+          isRequesting={isSignUpRequesting}
+        />
+      </form>
+    </div>
+  );
+};
+
+export default SignUpForm;
