16b57f4149c8c40c0d7b7ebbd348238c2a16834d
Angela <ang.hayrapetian@gmail.com>
2018-08-09 12:50:36 +0400

    [add] login validation helper

---
+++ src/helpers/loginValidate.js
@@ -0,0 +1,26 @@
+const loginValidate = values => {
+  const errors = {};
+  const requiredFields = ['username', 'password'];
+
+  requiredFields.forEach(field => {
+    if (!values[field]) {
+      errors[field] = `${field.charAt(0).toUpperCase() +
+        field.slice(1)} is required!`;
+    }
+
+    if (
+      field === 'email' &&
+      !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values[field])
+    ) {
+      errors[field] = 'Must be a valid email!';
+    }
+
+    if (values[field] && values[field].length < 5) {
+      errors[field] = 'Your input must contain more than 5 symbols!';
+    }
+  });
+
+  return errors;
+};
+
+export default loginValidate;
