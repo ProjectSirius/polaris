a282b9489e65f22291a8443ca84efa89d29a0417
Angela <ang.hayrapetian@gmail.com>
2018-08-10 22:13:37 +0400

    [add] price range render field

---
+++ src/components/PriceRangeSlider/RenderField.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { FormGroup, FormControl } from 'react-bootstrap';
+
+const TextField = ({ id, label, touched, error, ...props }) => {
+  return (
+    <div>
+      <FormGroup controlId={id}>
+        <FormControl {...props} />
+      </FormGroup>
+    </div>
+  );
+};
+
+const renderField = ({ input, meta: { touched, error }, ...custom }) => (
+  <div>
+    <TextField touched={touched} error={error} {...input} {...custom} />
+  </div>
+);
+
+export default renderField;
