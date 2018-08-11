72396e538f5ed3982e960d26e009b12cf27ae853
Angela <ang.hayrapetian@gmail.com>
2018-08-11 13:32:11 +0400

    [upd] Search

--- src/components/Search/styles.js
+++ src/components/Search/styles.js
@@ -1,11 +1,15 @@
 export default {
   searchWrapper: {
-    width: '80%',
+    width: '100%',
 
     '& > div': {
       margin: 0,
-      width: '90%',
+      width: '100%',
     },
+  },
+
+  search: {
+    height: '53px',
   },
 
   '@media screen and (max-width: 640px)': {
