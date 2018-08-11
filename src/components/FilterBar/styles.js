37d01b7f5661a41e00efd263804d4b89946fc42c
Angela <ang.hayrapetian@gmail.com>
2018-08-11 13:35:07 +0400

    [add] filter bar styles

---
+++ src/components/FilterBar/styles.js
@@ -0,0 +1,46 @@
+export default {
+  container: {
+    display: 'flex',
+  },
+
+  filterBar: {
+    /*width: '382px',*/
+  },
+
+  filterHeading: {
+    fontSize: '22px',
+    display: 'flex',
+  },
+
+  filterHeadingTitle: {
+    marginLeft: '11px',
+  },
+
+  filterWrapper: {
+    padding: '5px',
+    display: 'flex',
+    flexDirection: 'column',
+    height: '80px',
+    justifyContent: 'space-between',
+  },
+
+  filterTitle: {
+    fontSize: '20px',
+  },
+
+  column: {
+    padding: 0,
+  },
+
+  row: {
+    margin: 0,
+  },
+
+  searchRow: {
+    height: '53px',
+  },
+
+  configurable: {
+    display: 'flex',
+  },
+};
