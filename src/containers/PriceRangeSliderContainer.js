895bab9194fe20191f48aa8b4cfc4ffbdf9aa94d
Angela <ang.hayrapetian@gmail.com>
2018-08-11 13:18:49 +0400

    [add] price range initial values

--- src/containers/PriceRangeSliderContainer.js
+++ src/containers/PriceRangeSliderContainer.js
@@ -5,6 +5,10 @@ import PriceRangeSlider from '../components/PriceRangeSlider';
 
 const PriceRangeSliderForm = reduxForm({
   form: 'Price_range_form',
+  initialValues: {
+    min_price_range: 0,
+    max_price_range: 100,
+  },
 })(PriceRangeSlider);
 
 export default connect(
