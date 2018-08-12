c2de085ac6372dcddf12a7cb3342675578eebf04
Angela <ang.hayrapetian@gmail.com>
2018-08-12 14:06:09 +0400

    [add] checkbox filter styles

---
+++ src/components/CheckboxFilter/styles.js
@@ -0,0 +1,22 @@
+export default {
+  checkboxWrapper: {
+    padding: '1px 4px',
+  },
+
+  checkbox: {
+    opacity: 0,
+    position: 'absolute',
+  },
+
+  option: {
+    paddingLeft: '10px',
+  },
+
+  checked: {
+    opacity: 1,
+  },
+
+  unchecked: {
+    opacity: 0,
+  },
+};
