806826dedf616164d59da51bac911a82c5e3a7a1
Vrezh <hovanisyan.vrezh@gmail.com>
2018-08-13 17:59:22 +0400

    [add] styles for configurable and search component

---
+++ src/components/ConfigurableAndSearch/styles.js
@@ -0,0 +1,20 @@
+export default {
+  searchRow: {
+    display: 'flex',
+    padding: 0,
+    margin: 0,
+    '& > div': {
+      padding: 0,
+      margin: 0,
+    },
+  },
+
+  column: {
+    padding: 0,
+    margin: 0,
+  },
+
+  'right-column': {
+    marginLeft: '15px',
+  },
+};
