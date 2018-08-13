9568cd985e3a584a53c7ebd38d256e17db8e73ff
Vrezh <hovanisyan.vrezh@gmail.com>
2018-08-13 17:12:23 +0400

    [upd] selectors

--- src/selectors/index.js
+++ src/selectors/index.js
@@ -2,17 +2,16 @@ import { createSelector } from 'reselect';
 
 export const selectData = state => state.data;
 
-export {
-  selectIsSignUp,
-  selectIsAuth,
-};
-
 export const selectIsOpenMainMenu = state => state.isOpenMainMenu;
 export const selectIsRequesting = state => state.isRequesting;
 export const selectCurrentUser = state => state.currentUser;
 export const selectLanguage = state => state.lang;
 export const selectIsSignUp = state => state.isSignUp;
 export const selectError = state => state.error;
+export const selectFilteringPageRating = state => state.filteringPage.rating;
+export const selectFilteringPageFilterBar = state =>
+  state.filteringPage.filterBar;
+
 export const selectIsAuth = createSelector(
   selectCurrentUser,
   currentUser => currentUser !== null
