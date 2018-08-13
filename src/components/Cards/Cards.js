8f3ac97cb3a2752602495679e437dc954dfb7e1f
Vrezh <hovanisyan.vrezh@gmail.com>
2018-08-13 21:27:41 +0400

    [add] card layout

--- src/components/Cards/Cards.js
+++ src/components/Cards/Cards.js
@@ -1,7 +1,16 @@
 import React from 'react';
+import Card from '../Card';
 
-const Cards = ({ data, isRequesting }) => {
-  return isRequesting ? <h2>Requesting</h2> : <div>Data</div>;
+const Cards = ({ data, isRequesting, classes }) => {
+  return isRequesting ? (
+    <h2>Requesting</h2>
+  ) : (
+    <div className={classes.cardsWrapper}>
+      {data.map(info => (
+        <Card key={info.id} info={info} />
+      ))}
+    </div>
+  );
 };
 
 export default Cards;
