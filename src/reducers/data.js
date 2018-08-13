5c17fefba12a3c33d980120bc1aa8402981e30bb
Angela <ang.hayrapetian@gmail.com>
2018-08-13 16:41:02 +0400

    [change] initial state

--- src/reducers/data.js
+++ src/reducers/data.js
@@ -1,6 +1,6 @@
 import { SEARCH_SUCCESS } from '../actions/constants';
 
-const data = (state = {}, action) => {
+const data = (state = [], action) => {
   switch (action.type) {
     case SEARCH_SUCCESS:
       return action.payload.data;
