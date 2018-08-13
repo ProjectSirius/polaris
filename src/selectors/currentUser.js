79c5bb032b5655524fa5bc4235bdaeed724e87d5
Vrezh <hovanisyan.vrezh@gmail.com>
2018-08-13 17:11:32 +0400

    [del] unusable selectors

--- src/selectors/currentUser.js
+++
@@ -1,8 +0,0 @@
-import { createSelector } from 'reselect';
-
-const selectCurrentUser = createSelector(
-  state => state.currentUser,
-  currentUser => currentUser
-);
-
-export default selectCurrentUser;
--- src/selectors/filteringPage.js
+++
@@ -1,4 +0,0 @@
-const selectFilteringPageRating = state => state.filteringPage.rating;
-const selectFilteringPageFilterBar = state => state.filteringPage.filterBar;
-
-export { selectFilteringPageFilterBar, selectFilteringPageRating };
