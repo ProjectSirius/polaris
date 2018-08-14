a24d4489c859dcef2a3611da42fc90425c02ea6b
Angela <ang.hayrapetian@gmail.com>
2018-08-14 20:26:04 +0400

    [add] handle change to select filter

--- src/containers/SelectOptionFilter.js
+++ src/containers/SelectOptionFilter.js
@@ -1,7 +1,32 @@
+import React from 'react';
 import { connect } from 'react-redux';
 import { reduxForm } from 'redux-form';
+import { createStructuredSelector } from 'reselect';
+
+import search from '../actions/data';
+import { selectCurrentUser, selectIsAuth, selectOption } from '../selectors';
 
 import SelectOptionFilter from '../components/SelectOptionFilter';
+
+const SelectOptionFilterContainer = ({
+  search,
+  currentUser,
+  isAuth,
+  option,
+}) => {
+  const handleChange = (dataType, query, filter) => {
+    search(dataType, query, filter);
+  };
+
+  return (
+    <SelectOptionFilter
+      handleChange={handleChange}
+      currentUser={currentUser}
+      isAuth={isAuth}
+      option={option}
+    />
+  );
+};
 
 const SelectOptionFilterForm = reduxForm({
   form: 'SelectOptionFilter_form',
@@ -9,9 +34,20 @@ const SelectOptionFilterForm = reduxForm({
     selectedOption: '',
   },
   destroyOnUnmount: false,
-})(SelectOptionFilter);
+})(SelectOptionFilterContainer);
+
+const mapStateToProps = createStructuredSelector({
+  currentUser: selectCurrentUser,
+  isAuth: selectIsAuth,
+  option: selectOption,
+});
+
+const mapDispatchToProps = dispatch => ({
+  search: (dataType, query, filter) =>
+    dispatch(search(dataType, query, filter)),
+});
 
 export default connect(
-  null,
-  null
+  mapStateToProps,
+  mapDispatchToProps
 )(SelectOptionFilterForm);
