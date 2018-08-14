c8c1786a49d85026eebd7a873b73b223f858214e
Angela <ang.hayrapetian@gmail.com>
2018-08-14 09:39:55 +0400

    [add] list view styles

--- src/components/Cards/styles.js
+++ src/components/Cards/styles.js
@@ -1,8 +1,21 @@
 export default {
-  cardsWrapper: {
+  cardsWrapperGrid: {
     display: 'flex',
     flexWrap: 'wrap',
     justifyContent: 'space-between',
     margin: '15px 0',
+  },
+
+  /*********list view**********/
+  cardsWrapperList: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    margin: '15px 0',
+
+    '& > div': {
+      transition: 'all 0.6s ease-in-out',
+    },
   },
 };
