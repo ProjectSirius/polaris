4651c6704d62f5f4b1b7e6d39ee4519f661f974b
Angela <ang.hayrapetian@gmail.com>
2018-08-14 16:55:32 +0400

    [add] min and max prices selector

--- src/selectors/index.js
+++ src/selectors/index.js
@@ -11,10 +11,14 @@ export const selectFilteringPageRating = state => state.filteringPage.rating;
 export const selectFilteringPageFilterBar = state =>
   state.filteringPage.filterBar;
 export const selectRating = state => state.filterBar.rating;
-export const selectMinPrice = state => state.form.Price_range_form.values.min_price_range;
-export const selectMaxPrice = state => state.form.Price_range_form.values.max_price_range;
-export const selectMinInitalPrice = state => state.form.Price_range_form.initial.min_price_range;
-export const selectMaxInitalPrice = state => state.form.Price_range_form.initial.max_price_range;
+export const selectMinPrice = state =>
+  state.form.Price_range_form.values.min_price_range;
+export const selectMaxPrice = state =>
+  state.form.Price_range_form.values.max_price_range;
+export const selectMinInitalPrice = state =>
+  state.form.Price_range_form.initial.min_price_range;
+export const selectMaxInitalPrice = state =>
+  state.form.Price_range_form.initial.max_price_range;
 
 export const selectIsAuth = createSelector(
   selectCurrentUser,
