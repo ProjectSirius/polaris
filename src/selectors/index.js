abaccec97b133637f01ecb6204873c34a401abd7
Angela <ang.hayrapetian@gmail.com>
2018-08-16 01:28:36 +0400

    [upd] selectors

--- src/selectors/index.js
+++ src/selectors/index.js
@@ -1,5 +1,4 @@
 import { createSelector } from 'reselect';
-import { formValueSelector } from 'redux-form';
 
 export const selectData = state => state.data;
 export const selectIsOpenMainMenu = state => state.isOpenMainMenu;
@@ -11,22 +10,6 @@ export const selectError = state => state.error;
 export const selectFilteringPageRating = state => state.filteringPage.rating;
 export const selectFilteringPageFilterBar = state =>
   state.filteringPage.filterBar;
-export const selectRating = state => state.filterBar.rating;
-
-export const priceRangeSelector = formValueSelector('Price_range_form');
-export const selectOptionSelector = formValueSelector(
-  'SelectOptionFilter_form'
-);
-export const checkboxFilterSelector = formValueSelector('CheckboxFilter_form');
-
-export const selectMinPrice = state =>
-  priceRangeSelector(state, 'min_price_range');
-export const selectMaxPrice = state =>
-  priceRangeSelector(state, 'max_price_range');
-export const selectOption = state =>
-  selectOptionSelector(state, 'selectedOption');
-export const selectCheckboxOptions = state =>
-  checkboxFilterSelector(state, 'checkbox1', 'checkbox2', 'checkbox3');
 
 export const selectIsAuth = createSelector(
   selectCurrentUser,
