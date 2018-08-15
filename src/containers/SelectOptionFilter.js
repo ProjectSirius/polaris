8583a078e8c7758a24bba6936a8b751f6a1ceb62
Angela <ang.hayrapetian@gmail.com>
2018-08-16 01:59:57 +0400

    [add] set filters action to select option filter

--- src/containers/SelectOptionFilter.js
+++ src/containers/SelectOptionFilter.js
@@ -4,13 +4,21 @@ import { reduxForm } from 'redux-form';
 import { createStructuredSelector } from 'reselect';
 
 import search from '../actions/data';
+import { setFilters } from '../actions/';
+
 import { selectCurrentUser, selectIsAuth } from '../selectors';
 
 import SelectOptionFilter from '../components/SelectOptionFilter';
 
-const SelectOptionFilterContainer = ({ search, currentUser, isAuth }) => {
+const SelectOptionFilterContainer = ({
+  search,
+  currentUser,
+  isAuth,
+  setFilters,
+}) => {
   const handleChange = (dataType, query, filter) => {
-    search(dataType, query, filter);
+    setFilters(filter);
+    search(dataType, query);
   };
 
   return (
@@ -38,6 +46,7 @@ const mapStateToProps = createStructuredSelector({
 const mapDispatchToProps = dispatch => ({
   search: (dataType, query, filter) =>
     dispatch(search(dataType, query, filter)),
+  setFilters: filter => dispatch(setFilters(filter)),
 });
 
 export default connect(
