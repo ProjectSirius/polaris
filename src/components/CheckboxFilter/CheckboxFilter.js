3f1e8b48973fa5e975458a6c880d9cbc1272ac88
Angela <ang.hayrapetian@gmail.com>
2018-08-15 16:49:26 +0400

    [add] connection with search

--- src/components/CheckboxFilter/CheckboxFilter.js
+++ src/components/CheckboxFilter/CheckboxFilter.js
@@ -3,38 +3,64 @@ import { Field } from 'redux-form';
 
 import renderField from './RenderField';
 
-const CheckboxFilter = ({ classes }) => (
-  <div>
+const CheckboxFilter = ({ classes, handleChange, isAuth, currentUser }) => {
+  const dataType = isAuth
+    ? currentUser.userType === 'content_owner'
+      ? 'channels'
+      : 'contents'
+    : '';
+
+  return (
     <div>
-      <Field
-        name="checkbox1"
-        id="1"
-        component={renderField}
-        type="checkbox"
-        label="Option One"
-        classes={classes}
-      />
+      <div>
+        <Field
+          name="checkbox1"
+          id="1"
+          component={renderField}
+          type="checkbox"
+          label="Option One"
+          classes={classes}
+          onChange={e =>
+            handleChange(dataType, '', {
+              filterName: 'checkbox1',
+              value: e.target.checked,
+            })
+          }
+        />
+      </div>
+      <div>
+        <Field
+          name="checkbox2"
+          id="2"
+          component={renderField}
+          type="checkbox"
+          label="Option Two"
+          classes={classes}
+          onChange={e =>
+            handleChange(dataType, '', {
+              filterName: 'checkbox2',
+              value: e.target.checked,
+            })
+          }
+        />
+      </div>
+      <div>
+        <Field
+          name="checkbox3"
+          id="3"
+          component={renderField}
+          type="checkbox"
+          label="Option Three"
+          classes={classes}
+          onChange={e =>
+            handleChange(dataType, '', {
+              filterName: 'checkbox3',
+              value: e.target.checked,
+            })
+          }
+        />
+      </div>
     </div>
-    <div>
-      <Field
-        name="checkbox2"
-        id="2"
-        component={renderField}
-        type="checkbox"
-        label="Option Two"
-        classes={classes}
-      />
-    </div>
-    <div>
-      <Field
-        name="checkbox3"
-        id="3"
-        component={renderField}
-        type="checkbox"
-        label="Option Three"
-        classes={classes}
-      />
-    </div>
-  </div>
-);
+  );
+};
 export default CheckboxFilter;
--- src/containers/CheckboxFilter.js
+++ src/containers/CheckboxFilter.js
@@ -1,7 +1,26 @@
+import React from 'react';
 import { connect } from 'react-redux';
 import { reduxForm } from 'redux-form';
+import { createStructuredSelector } from 'reselect';
+
+import { selectCurrentUser, selectIsAuth } from '../selectors';
+
+import search from '../actions/data';
 
 import CheckboxFilter from '../components/CheckboxFilter';
+
+const CheckboxFilterContainer = ({ currentUser, isAuth }) => {
+  const handleChange = (dataType, query, filter) => {
+    search(dataType, query, filter);
+  };
+  return (
+    <CheckboxFilter
+      handleChange={handleChange}
+      currentUser={currentUser}
+      isAuth={isAuth}
+    />
+  );
+};
 
 const CheckboxFilterForm = reduxForm({
   form: 'CheckboxFilter_form',
@@ -11,9 +30,19 @@ const CheckboxFilterForm = reduxForm({
     checkbox3: false,
   },
   destroyOnUnmount: false,
-})(CheckboxFilter);
+})(CheckboxFilterContainer);
+
+const mapStateToProps = createStructuredSelector({
+  currentUser: selectCurrentUser,
+  isAuth: selectIsAuth,
+});
+
+const mapDispatchToProps = dispatch => ({
+  search: (dataType, query, filter) =>
+    dispatch(search(dataType, query, filter)),
+});
 
 export default connect(
-  null,
-  null
+  mapStateToProps,
+  mapDispatchToProps
 )(CheckboxFilterForm);
