882346ebe1d11abb1d6a693d486cb4748ce95689
Vrezh <hovanisyan.vrezh@gmail.com>
2018-08-16 02:56:20 +0400

    [upd] card layout

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,56 +1,24 @@
 import React from 'react';
-import { Label } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
 import { Glyphicon } from 'react-bootstrap';
 
-const Card = ({
-  info: { id, title, description, date, img },
-  classes,
-  view,
-}) => {
+const Card = ({ info: { id, title, description, date, img }, classes }) => {
   return (
-    <div
-      className={
-        view === 'table' ? classes.cardWrapper : classes.cardWrapperList
-      }
-    >
-      <div className={view === 'table' ? classes.card : classes.cardList}>
-        <div className={view === 'list' ? classes.imgWrapper : ''}>
-          <Link to={`channels/${id}`}>
-            <div className={classes.imgModal}>
-              <Link to={`channels/${id}`}>
-                <div className={classes.icon}>
-                  <Glyphicon glyph="glyphicon glyphicon-share-alt" />
-                </div>
-              </Link>
-            </div>
-            <img
-              className={view === 'table' ? classes.img : classes.imgList}
-              src="https://skirtingboardsdirect.com/wp-content/uploads/2014/12/Fallback-Image-600x600.png"
-              alt="Sorry, we can't upload!"
-            />
-          </Link>
-        </div>
-        <article
-          className={view === 'table' ? classes.content : classes.contentList}
-        >
-          <h2 className={classes.title}>{title}</h2>
-          <div className={classes.countryTime}>
-            <span>from Armenia</span>
-            <span>{date}</span>
-          </div>
-          <div className={classes.description}>
-            <p className={classes.descriptionText}>
-              {`${description.slice(0, 181)}..`}
-            </p>
-          </div>
-          <div className={classes.tags}>
-            <Label bsStyle="warning">Warning</Label>{' '}
-            <Label bsStyle="danger">Danger</Label>{' '}
-          </div>
-        </article>
+    <article className={classes.card}>
+      <header className={classes.cardHeader}>
+        <h4 className={classes.cardHeaderTitle}>Think Different</h4>
+      </header>
+      <div className={classes.cardBody}>
+        <p className={classes.date}>{date}</p>
+        <h3 className={classes.mainTitle}>{title}</h3>
+        <p className={classes.bodyContent}>
+          {description.slice(0, 180)}
+          ..
+        </p>
       </div>
-    </div>
+      <button className={`${classes.button} ${classes.buttonPrimary}`}>
+        <Glyphicon glyph="glyphicon glyphicon-chevron-right" /> Find out more
+      </button>
+    </article>
   );
 };
 
