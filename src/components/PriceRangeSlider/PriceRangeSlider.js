bdaf9031145c9f7833873e9377be7ddf8bbba3f1
Vrezh <hovanisyan.vrezh@gmail.com>
2018-08-16 06:26:25 +0400

    [upd] price range slider styles

--- src/components/PriceRangeSlider/PriceRangeSlider.js
+++ src/components/PriceRangeSlider/PriceRangeSlider.js
@@ -3,26 +3,28 @@ import { Field } from 'redux-form';
 
 import RenderField from './RenderField';
 
-const PriceRangeSlider = ({ classes }) => {
+const PriceRangeSlider = ({ classes, minPrice, maxPrice }) => {
   return (
     <div className={classes.priceSlider}>
       <Field
         name="min_price_range"
         min="0"
         max="100"
-        step="0.5"
+        step="1"
         type="range"
         className={classes.slider}
         component={RenderField}
+        value={minPrice}
       />
       <Field
         name="max_price_range"
         min="0"
         max="100"
-        step="0.5"
+        step="1"
         type="range"
         className={classes.slider}
         component={RenderField}
+        value={maxPrice}
       />
     </div>
   );
