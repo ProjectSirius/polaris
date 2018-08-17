444787f36be6c2d83f961d7108d5b5d59912d3d8
Angela <ang.hayrapetian@gmail.com>
2018-08-17 22:20:25 +0400

    [add] tag styles

--- src/components/CreateChannel/styles.js
+++ src/components/CreateChannel/styles.js
@@ -73,20 +73,74 @@ export default {
     cursor: 'pointer',
   },
 
-  tag: {
-    display: 'inline-block',
-    padding: '2px',
-    margin: '2px 0',
-    background: '#FFF',
-    border: '1px solid #CCC',
-    width: '16em',
-    font: 'inherit',
-    borderRadius: '2px',
-    boxShadow: 'inset 0 1px 2px rgba(0, 0, 0, .1)',
+  tags: {
+    '& .control-label': {
+      width: '100%',
 
-    '&:focus': {
-      borderColor: '#59F',
-      outline: 'none',
+      '& > .react-tags': {
+        display: 'flex',
+
+        '& > .react-tags__container': {
+          listStyle: 'none',
+          display: 'flex',
+          padding: 0,
+
+          '& li': {
+            display: 'flex',
+            justifyContent: 'space-between',
+            padding: '3px',
+            background: '#ea585f',
+            margin: '2px',
+            fontWeight: 600,
+            borderRadius: '20px',
+            overflow: 'hidden',
+            height: '27px',
+            position: 'relative',
+            color: '#fff',
+            minWidth: '80px',
+
+            '& a': {
+              width: '24%',
+              justifyContent: 'center',
+              display: 'flex',
+              background: '#a94442',
+              marginLeft: '5px',
+              color: '#fff',
+              height: '32px',
+              position: 'absolute',
+              right: 0,
+              top: '-3px',
+              lineHeight: '33px',
+              textDecoration: 'none',
+              cursor: 'pointer',
+              fontSize: '18px',
+            },
+          },
+        },
+
+        '& input[type="text"]': {
+          fontWeight: 400,
+          display: 'block',
+          width: '100%',
+          height: '34px',
+          padding: '6px 12px',
+          fontSize: '14px',
+          lineHeight: '1.42857143',
+          color: '#555',
+          backgroundColor: '#fff',
+          backgroundImage: 'none',
+          border: '1px solid #ccc',
+          borderRadius: '4px',
+          boxShadow: 'inset 0 1px 1px rgba(0, 0, 0, .075)',
+          transition:
+            'border-color ease-in-out .15s,box-shadow ease-in-out .15s',
+          WebkitBoxShadow: 'inset 0 1px 1px rgba(0, 0, 0, .075)',
+          WebkitTransition:
+            'border-color ease-in-out .15s,-webkit-box-shadow ease-in-out .15s',
+          OTransition:
+            'border-color ease-in-out .15s,box-shadow ease-in-out .15s',
+        },
+      },
     },
   },
 };
