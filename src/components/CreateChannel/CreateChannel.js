b1305de0910d36f7c64e48574da4f0814b42054b
Angela <ang.hayrapetian@gmail.com>
2018-08-17 17:22:59 +0400

    [add] new fields to channel form

--- src/components/CreateChannel/CreateChannel.js
+++ src/components/CreateChannel/CreateChannel.js
@@ -3,54 +3,75 @@ import { Field } from 'redux-form';
 
 import RenderField from './RenderField';
 import renderField from '../SelectOptionFilter/RenderField';
-import fileRenderField from './FileRenderField';
+import SubmitBtn from '../SubmitBtn';
 
-const CreateChannel = ({ title, classes }) => {
+const CreateChannel = ({ isRequesting, classes, valid }) => {
   return (
     <div className={classes.newChannelWrapper}>
-      <div className={classes.title}>Create New Channel</div>
-      <form action="" className={classes.form}>
-        <Field
-          component={RenderField}
-          name="title"
-          placeholder="Channel Title"
-          className={classes.inputFiled}
-          type="text"
-        />
-        <Field
-          component={RenderField}
-          name="detailedText"
-          placeholder="Detailed description"
-          className={`${classes.inputFiled} ${classes.textarea}`}
-          componentClass="textarea"
-        />
-        <div className={classes.minAndMaxPrice}>
-          <Field
-            component={RenderField}
-            name="MinPrice"
-            placeholder="Min Price"
-            type="number"
-          />
-          <Field
-            component={RenderField}
-            name="MaxPrice"
-            placeholder="Max Price"
-            type="number"
-          />
+      <div>
+        <div className={classes.titleWrapper}>
+          <div className={classes.title}>Create New Channel</div>
         </div>
-        <Field name="selectedOption" component={renderField}>
-          <option>{'Select an option'}</option>
-          <option>{'Option 1'}</option>
-          <option>{'Option 2'}</option>
-          <option>{'Option 3'}</option>
-        </Field>
-        <Field
-          component={fileRenderField}
-          name="file"
-          type="file"
-          classes={classes}
-        />
-      </form>
+        <form
+          action="https://acampapi.haffollc.com/v1/channels"
+          method="post"
+          className={classes.form}
+          encType="multipart/form-data"
+        >
+          <Field
+            label="Channel Title:"
+            component={RenderField}
+            name="title"
+            className={classes.inputFiled}
+            type="text"
+          />
+          <Field
+            label="Brief Description:"
+            component={RenderField}
+            name="description"
+            className={`${classes.inputFiled} `}
+            componentClass="textarea"
+          />
+          <Field
+            label="Detailed Description:"
+            component={RenderField}
+            name="detailedText"
+            className={`${classes.inputFiled} ${classes.textarea}`}
+            componentClass="textarea"
+          />
+          <div className={classes.minAndMaxPrice}>
+            <Field
+              label="Price:"
+              component={RenderField}
+              name="price"
+              type="number"
+              min="1"
+            />
+            <Field
+              label="Per:"
+              component={RenderField}
+              name="count"
+              placeholder="1000"
+              type="number"
+            />
+            <Field label="Unit" name="unit" component={renderField}>
+              <option>{'View'}</option>
+              <option>{'Finshed minute'}</option>
+              <option>{'Like'}</option>
+            </Field>
+          </div>
+          <Field
+            label="Link Of Your Channel:"
+            component={RenderField}
+            type="url"
+            name="url"
+            placeholder="https://example.com"
+            pattern="https://.*"
+          />
+          <Field label="Tags:" component={RenderField} type="tags" name="tag" />
+          <SubmitBtn value="Submit" valid={valid} isRequesting={isRequesting} />
+        </form>
+      </div>
     </div>
   );
 };
