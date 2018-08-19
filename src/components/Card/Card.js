35b6af4aed91114659b1d5c2aac95cc232f67ee6
Angela <ang.hayrapetian@gmail.com>
2018-08-20 01:08:20 +0400

    [fix] name issue

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Glyphicon } from 'react-bootstrap';
 
-const Card = ({ info: { title, description, date }, classes, view }) => {
+const Card = ({ info: { title, briefDescription, date }, classes, view }) => {
   return (
     <article
       className={`${classes.card} ${view === 'list' ? classes.cardList : ''}`}
@@ -17,7 +17,9 @@ const Card = ({ info: { title, description, date }, classes, view }) => {
         <p className={classes.date}>{date}</p>
         <h3 className={classes.mainTitle}>{title}</h3>
         <p className={classes.bodyContent}>
-          {view === 'list' ? description : `${description.slice(0, 180)}..`}
+          {view === 'list'
+            ? briefDescription
+            : `${briefDescription.slice(0, 180)}..`}
         </p>
       </div>
       {view === 'list' ? (
