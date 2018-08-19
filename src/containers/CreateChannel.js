9f4a9fb3f046d92641307eb9511cd0f760bfbb91
Angela <ang.hayrapetian@gmail.com>
2018-08-19 22:56:07 +0400

    [add] set form initial values

--- src/containers/CreateChannel.js
+++ src/containers/CreateChannel.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { reduxForm } from 'redux-form';
 import { injectIntl } from 'react-intl';
@@ -9,34 +9,53 @@ import {
   selectTags,
   selectIsAuth,
   selectCurrentUser,
+  selectDetailed,
+  selectIsEditing,
 } from '../selectors';
 
-import { addTags, removeTags, sendData } from '../actions';
+import { addTags, removeTags, sendData, getDetails } from '../actions';
 
 import CreateCard from '../components/CreateCard';
 
 import channelFormValidate from '../helpers/channelFormValidate';
 import messages from '../helpers/contentChannelFormMessages';
 
-let CreateChannelContainer = props => {
-  const {
-    intl: { formatMessage },
-  } = props;
+class CreateChannelContainer extends Component {
+  componentDidMount() {
+    const dataType = 'channels';
 
-  const onFormSubmit = formData => {
-    props.sendData(formData, 'createChannel');
+    this.props.getDetails(dataType, '1');
+  }
+
+  onFormSubmit = formData => {
+    this.props.sendData(formData, 'createChannel');
   };
 
-  return (
-    <CreateCard
-      messages={messages}
-      formatMessage={formatMessage}
-      {...props}
-      onFormSubmit={onFormSubmit}
-      userType="audience_owner"
-    />
-  );
-};
+  getData = () => {
+    this.props.getDetails('channels', '1');
+  };
+
+  render() {
+    const {
+      intl: { formatMessage },
+      data,
+      isEditing,
+    } = this.props;
+
+    return (
+      <CreateCard
+        messages={messages}
+        formatMessage={formatMessage}
+        onFormSubmit={this.onFormSubmit}
+        userType="audience_owner"
+        data={data}
+        isEditing={isEditing}
+        getData={this.getData}
+        {...this.props}
+      />
+    );
+  }
+}
 
 CreateChannelContainer = injectIntl(CreateChannelContainer);
 
@@ -45,15 +64,16 @@ const mapStateToProps = createStructuredSelector({
   tags: selectTags,
   isAuth: selectIsAuth,
   currentUser: selectCurrentUser,
+  data: selectDetailed,
+  isEditing: selectIsEditing,
+  initialValues: selectDetailed,
 });
 
 const addNewChannelForm = reduxForm({
   form: 'Add_new_channel_form',
   validate: channelFormValidate,
-  initialValues: {
-    unit: 'view',
-  },
   destroyOnUnmount: false,
+  enableReinitialize: true,
 })(CreateChannelContainer);
 
 export default connect(
@@ -62,5 +82,6 @@ export default connect(
     addTags,
     removeTags,
     sendData,
+    getDetails,
   }
 )(addNewChannelForm);
