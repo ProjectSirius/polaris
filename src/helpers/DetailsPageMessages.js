98f54425c28c3e228ca23b321a79f90654ec1085
Angela <ang.hayrapetian@gmail.com>
2018-08-20 16:57:45 +0400

    [add] details page messages

---
+++ src/helpers/DetailsPageMessages.js
@@ -0,0 +1,70 @@
+import { defineMessages } from 'react-intl';
+
+const messages = defineMessages({
+  channelTagsTitle: {
+    id: 'channel-tags-title',
+    defaultMessage: 'Channel Tags',
+  },
+
+  contentTagsTitle: {
+    id: 'content-tags-title',
+    defaultMessage: 'Content Tags',
+  },
+
+  averageRatingTitle: {
+    id: 'average-rating-title',
+    defaultMessage: 'Average Rating',
+  },
+
+  editChannel: {
+    id: 'edit-channel',
+    defaultMessage: 'Edit Channel',
+  },
+
+  editContent: {
+    id: 'edit-content',
+    defaultMessage: 'Edit Content',
+  },
+
+  deleteChannel: {
+    id: 'delete-channel',
+    defaultMessage: 'Delete Channel',
+  },
+
+  deleteContent: {
+    id: 'delete-content',
+    defaultMessage: 'Delete Content',
+  },
+
+  briefDescriptionTitle: {
+    id: 'brief-description',
+    defaultMessage: 'Brief Description:',
+  },
+
+  detailedDescriptionTitle: {
+    id: 'brief-description',
+    defaultMessage: 'Detailed Description:',
+  },
+
+  musicTitle: {
+    id: 'music-title',
+    defaultMessage: 'Piece Of Music',
+  },
+
+  locationTitle: {
+    id: 'location-title',
+    defaultMessage: 'Location:',
+  },
+
+  offerTitle: {
+    id: 'offer-title',
+    defaultMessage: 'Make An Offer',
+  },
+
+  channelLinks: {
+    id: 'channel-links',
+    defineMessages: 'Channel Links:',
+  },
+});
+
+export default messages;
