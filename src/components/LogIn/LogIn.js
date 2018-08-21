d33da35f82f411f6178edacd75f0e0f838a24e3b
Vrezh <hovanisyan.vrezh@gmail.com>
2018-08-21 14:38:49 +0400

    [upd] login form

--- src/components/LogIn/LogIn.js
+++ src/components/LogIn/LogIn.js
@@ -4,6 +4,7 @@ import { Redirect, Link } from 'react-router-dom';
 import { injectIntl, defineMessages } from 'react-intl';
 import PropTypes from 'prop-types';
 import { Alert } from 'react-bootstrap';
+import { Form, Grid, Message, Icon } from 'semantic-ui-react';
 
 import RenderField from './RenderField';
 import SubmitBtn from '../SubmitBtn';
@@ -53,43 +54,60 @@ const LogIn = ({
     <Redirect to={from} />
   ) : (
     <div className={classes.loginFormWrapper}>
-      <h1 className={classes.loginTitle}>
-        {formatMessage(messages.loginForm)}
-      </h1>
-      <div className={classes.loginForm}>
-        <form onSubmit={handleSubmit(login)}>
-          <Field
-            name="username"
-            component={RenderField}
-            type="text"
-            bsSize="large"
-            placeholder="Your Username"
-          />
-          <Field
-            name="password"
-            component={RenderField}
-            type="password"
-            placeholder="Your Password"
-            bsSize="large"
-          />
-          {isRequesting && (
-            <Alert bsStyle="info">
-              {formatMessage(messages.requestingAlert)}
-            </Alert>
-          )}
-          {loginError && <Alert bsStyle="danger">{loginError}</Alert>}
-          <SubmitBtn value="Log In" valid={valid} isRequesting={isRequesting} />
-          <div className={classes.signUpDesc}>
-            <p>New to Polaris?</p>
+      <Grid columns={16} centered>
+        <Grid.Column computer={8} mobile={16}>
+          <Message attached>
+            <Message.Header className={classes.messageHeaderText}>
+              {formatMessage(messages.loginForm)}
+            </Message.Header>
+            <Message.Content className={classes.messageText}>
+              Fill out the form below to log-in!
+            </Message.Content>
+          </Message>
+          <Form onSubmit={handleSubmit(login)} className="attached segment">
+            <Field
+              name="username"
+              component={RenderField}
+              type="text"
+              placeholder="Your Username"
+              classes={classes}
+            />
+            <Field
+              name="password"
+              component={RenderField}
+              type="password"
+              placeholder="Your Password"
+              classes={classes}
+            />
+            {isRequesting && (
+              <Alert bsStyle="info">
+                {formatMessage(messages.requestingAlert)}
+              </Alert>
+            )}
+            {loginError && <Alert bsStyle="danger">{loginError}</Alert>}
+            <SubmitBtn
+              value="Log In"
+              valid={valid}
+              isRequesting={isRequesting}
+            />
+          </Form>
+          <Message attached="bottom" warning>
+            <Icon name="help" />
+            New to Polaris?
             <p>
               Sign up as{' '}
               <Link
-                to={{ pathname: `/signupAudience`, search: `?locale=${lang}` }}
+                className={classes.link}
+                to={{
+                  pathname: `/signupAudience`,
+                  search: `?locale=${lang}`,
+                }}
               >
                 {formatMessage(messages.audienceLogin)}
               </Link>{' '}
               or{' '}
               <Link
+                className={classes.link}
                 to={{
                   pathname: `/signupContentmaker`,
                   search: `?locale=${lang}`,
@@ -99,9 +117,9 @@ const LogIn = ({
               </Link>{' '}
               owner!
             </p>
-          </div>
-        </form>
-      </div>
+          </Message>
+        </Grid.Column>
+      </Grid>
     </div>
   );
 };
--- src/components/LogIn/RenderField.js
+++ src/components/LogIn/RenderField.js
@@ -1,17 +1,12 @@
 import React from 'react';
-import { FormGroup, FormControl } from 'react-bootstrap';
+import { Form, Label } from 'semantic-ui-react';
 
-const TextField = ({ id, label, touched, error, ...props }) => {
+const TextField = ({ id, label, touched, error, classes, ...props }) => {
   return (
-    <React.Fragment>
-      <FormGroup
-        validationState={touched && error ? 'error' : null}
-        controlId={id}
-      >
-        <FormControl {...props} />
-        {touched && error ? <span className="feedbackError">{error}</span> : ''}
-      </FormGroup>
-    </React.Fragment>
+    <Form.Field className={classes.fieldReset} widths="equal">
+      <Form.Input {...props} />
+      {touched && error ? <Label pointing>{error}</Label> : ''}
+    </Form.Field>
   );
 };
 
@@ -20,6 +15,14 @@ const renderField = ({
   meta: { touched, error },
   classes,
   ...custom
-}) => <TextField touched={touched} error={error} {...input} {...custom} />;
+}) => (
+  <TextField
+    classes={classes}
+    touched={touched}
+    error={error}
+    {...input}
+    {...custom}
+  />
+);
 
 export default renderField;
