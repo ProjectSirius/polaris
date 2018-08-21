7128c4bc4fe3feb8dd932796a6807ade90a8591b
Angela <ang.hayrapetian@gmail.com>
2018-08-22 00:38:42 +0400

    [del] unnecessary styles

--- src/components/StarRating/styles.js
+++ src/components/StarRating/styles.js
@@ -1,38 +1,6 @@
 export default {
   rating: {
-    lineHeight: 1,
     display: 'flex',
-    padding: '5px 0',
     justifyContent: 'center',
-    cursor: 'pointer',
-  },
-
-  defaultStar: {
-    '&:hover span': {
-      color: '#ED1C24',
-    },
-
-    cursor: 'pointer',
-    fontSize: '30px',
-    padding: '5px',
-  },
-
-  rateStar: {
-    '& span': {
-      color: '#ED1C24',
-    },
-
-    cursor: 'pointer',
-    fontSize: '30px',
-    padding: '5px',
-  },
-
-  '@media screen and (max-width: 780px)': {
-    rateStar: {
-      fontSize: '25px',
-    },
-    defaultStar: {
-      fontSize: '25px',
-    },
   },
 };
