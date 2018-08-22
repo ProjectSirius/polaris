7a2dca057ad3124803c74e8f8bd81372bce8a6d9
Angela <ang.hayrapetian@gmail.com>
2018-08-22 14:23:54 +0400

    [add] details page container

--- src/containers/DetailsPageContainer.js
+++ src/containers/DetailsPageContainer.js
@@ -1,26 +1,72 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { injectIntl, defineMessages } from 'react-intl';
+import { injectIntl } from 'react-intl';
+import { createStructuredSelector } from 'reselect';
+
+import { getDetails, edit } from '../actions';
+
+import {
+  selectDetailed,
+  selectIsRequesting,
+  selectLanguage,
+  selectCurrentUser,
+} from '../selectors';
 
 import DetailedPage from '../components/DetailsPage';
 
-const messages = defineMessages({
-  title: {
-    id: 'detailed-page-title',
-    defaultMessage: 'Detailed Page',
-  },
+import messages from '../helpers/detailsPageMessages';
+
+class DetailsPageContainer extends React.Component {
+  componentDidMount() {
+    const dataType =
+      this.props.currentUser.userType === 'audience_owner'
+        ? 'channels'
+        : 'contents';
+    const id = this.props.match.params.id;
+
+    this.props.getDetails(dataType, id);
+  }
+
+  handleEdit = () => {
+    this.props.edit();
+  };
+
+  render() {
+    const {
+      intl: { formatMessage },
+      isRequesting,
+      detailed,
+      lang,
+      currentUser,
+    } = this.props;
+
+    return (
+      <DetailedPage
+        data={detailed}
+        isRequesting={isRequesting}
+        lang={lang}
+        handleEdit={this.handleEdit}
+        userType={currentUser.userType}
+        messages={messages}
+        formatMessage={formatMessage}
+      />
+    );
+  }
+}
+
+const mapStateToProps = createStructuredSelector({
+  detailed: selectDetailed,
+  isRequesting: selectIsRequesting,
+  lang: selectLanguage,
+  currentUser: selectCurrentUser,
 });
 
-let HomePageContainer = props => {
-  const {
-    intl: { formatMessage },
-  } = props;
+DetailsPageContainer = injectIntl(DetailsPageContainer);
 
-  const formattedTitle = formatMessage(messages.title);
-
-  return <DetailedPage title={formattedTitle} />;
-};
-
-HomePageContainer = injectIntl(HomePageContainer);
-
-export default connect()(HomePageContainer);
+export default connect(
+  mapStateToProps,
+  {
+    getDetails,
+    edit,
+  }
+)(DetailsPageContainer);
