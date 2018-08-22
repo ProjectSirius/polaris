3dcde6d347531b747dc14b2c3359ad7d8f8918d8
Vrezh <hovanisyan.vrezh@gmail.com>
2018-08-22 23:53:23 +0400

    [upd] notification view and functionality

--- src/components/Notifications/styles.js
+++ src/components/Notifications/styles.js
@@ -1,10 +1,34 @@
 export default {
-  notifs: {
-    width: '300px !important',
+  dropdownParent: {
+    paddingTop: '1.2em !important',
+    textAlign: 'center !important',
+
+    '@media screen and (max-width: 768px)': {
+      paddingBottom: 20,
+    },
+
+    '&:hover': {
+      borderBottom: '2px solid #000',
+      marginBottom: '-2px',
+    },
+
+    '&.ui.scrolling.dropdown .menu': {
+      width: '500px !important',
+      minWidth: 'auto !important',
+
+      '@media screen and (max-width: 768px)': {
+        width: '100% !important',
+        maxHeight: window.innerHeight - 100,
+      },
+    },
   },
 
   notifWrapper: {
-    padding: '15px !important',
-    width: '300px !important',
+    fontSize: '16px !important',
+
+    '& .date': {
+      marginBottom: '5px !important',
+      fontSize: '14px !important',
+    },
   },
 };
