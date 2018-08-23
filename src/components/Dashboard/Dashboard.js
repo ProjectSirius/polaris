67b6915bce8df408c92ed7c0a7c6270d8897f554
Vrezh <hovanisyan.vrezh@gmail.com>
2018-08-23 15:17:19 +0400

    [upd] dashboard view on small screens

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,18 +1,34 @@
 import React from 'react';
-import { Tab } from 'semantic-ui-react';
-import TransactionCards from '../TransactionCards';
+import { Tab, Responsive } from 'semantic-ui-react';
+
+import TransactionCardsContainer from '../../containers/TransactionCardsContainer';
 
 const panes = [
   {
     menuItem: 'Transactions',
-    render: () => <TransactionCards />,
+    render: () => <TransactionCardsContainer />,
   },
   { menuItem: 'Payment', render: () => <Tab.Pane>Tab 2 Content</Tab.Pane> },
   { menuItem: 'My Channels', render: () => <Tab.Pane>Tab 3 Content</Tab.Pane> },
 ];
 
-const TabExampleVerticalTabular = () => (
-  <Tab menu={{ fluid: true, vertical: true, tabular: true }} panes={panes} />
-);
+const TabExampleVerticalTabular = () => {
+  return (
+    <React.Fragment>
+      <Responsive minWidth="768">
+        <Tab
+          menu={{ fluid: true, vertical: true, tabular: true }}
+          panes={panes}
+        />
+      </Responsive>
+      <Responsive maxWidth="768">
+        <Tab
+          menu={{ fluid: true, vertical: false, tabular: true }}
+          panes={panes}
+        />
+      </Responsive>
+    </React.Fragment>
+  );
+};
 
 export default TabExampleVerticalTabular;
