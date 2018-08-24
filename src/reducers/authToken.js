0c2cd37e73386db5bdf2b2a73279cc46bb318ea1
Vrezh <hovanisyan.vrezh@gmail.com>
2018-08-25 00:40:03 +0400

    [add] deleting auth token on log out

--- src/reducers/authToken.js
+++ src/reducers/authToken.js
@@ -1,9 +1,11 @@
-import { SAVE_TOKEN } from '../actions/constants';
+import { SAVE_TOKEN, LOGOUT_SUCCESS } from '../actions/constants';
 
 const authToken = (state = '', action) => {
   switch (action.type) {
     case SAVE_TOKEN:
       return action.payload.token;
+    case LOGOUT_SUCCESS:
+      return state;
     default:
       return state;
   }
