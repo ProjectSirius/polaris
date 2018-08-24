146b31d8fbceb74541f5fa6f008f9975e48474de
Vrezh <hovanisyan.vrezh@gmail.com>
2018-08-24 15:03:08 +0400

    [add] visibility of nav bar

--- src/components/MainNavBar/styles.js
+++ src/components/MainNavBar/styles.js
@@ -9,6 +9,7 @@ export default {
   mainNavBar: {
     backgroundColor: 'rgba(255, 255, 255, 1) !important',
     padding: '0 30px',
+    zIndex: '1000 !important',
 
     '& > .item, & > dropdown': {
       padding: '1.2em !important',
