578608b7f10ba48745c4f0b6fce39e9b60a70618
Angela <ang.hayrapetian@gmail.com>
2018-08-27 09:49:07 +0400

    [change] group offer initial state

--- src/reducers/groupOffer.js
+++ src/reducers/groupOffer.js
@@ -3,12 +3,26 @@ import {
   REMOVE_FROM_GROUP_OFFER,
 } from '../actions/constants';
 
-const groupOffer = (state = [], action) => {
+const initialState = {
+  buyer_id: null,
+  selectedIds: [],
+  options: {},
+};
+
+const groupOffer = (state = initialState, action) => {
   switch (action.type) {
     case ADD_TO_GROUP_OFFER:
-      return [...state, action.payload.id];
+      return {
+        ...state,
+        buyer_id: action.payload.buyer_id,
+        selectedIds: [...state.selectedIds, action.payload.id],
+      };
     case REMOVE_FROM_GROUP_OFFER:
-      return state.filter(el => el !== action.payload.id);
+      return {
+        ...state,
+        buyer_id: action.payload.buyer_id,
+        selectedIds: state.selectedIds.filter(el => el !== action.payload.id),
+      };
     default:
       return state;
   }
