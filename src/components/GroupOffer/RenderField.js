7e595d7a053b39212b08835705d81c1dd1640b14
Angela <ang.hayrapetian@gmail.com>
2018-08-27 09:47:34 +0400

    [add] group form render filed

---
+++ src/components/GroupOffer/RenderField.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Form, Label } from 'semantic-ui-react';
+
+const TextField = ({ id, icon, label, touched, error, classes, ...props }) => {
+  return (
+    <Form.Field widths="equal">
+      <label>{label}:</label>
+      <Form.Input icon={icon ? icon : ''} iconPosition="left" {...props} />
+      {touched && error ? <Label pointing>{error}</Label> : ''}
+    </Form.Field>
+  );
+};
+
+const renderField = ({
+  input,
+  classes,
+  meta: { touched, error },
+  ...custom
+}) => (
+  <TextField
+    touched={touched}
+    classes={classes}
+    error={error}
+    {...input}
+    {...custom}
+  />
+);
+
+export default renderField;
