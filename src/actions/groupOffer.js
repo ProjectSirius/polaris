4497f50b899fd85b9b75ace73c69560985382794
Angela <ang.hayrapetian@gmail.com>
2018-08-27 09:48:38 +0400

    [add] buyer id to group offer

--- src/actions/groupOffer.js
+++ src/actions/groupOffer.js
@@ -1,11 +1,21 @@
 import { ADD_TO_GROUP_OFFER, REMOVE_FROM_GROUP_OFFER } from './constants';
 
-export const addToGroupOffer = id => ({
+export const addOffer = (id, buyer_id) => ({
   type: ADD_TO_GROUP_OFFER,
-  payload: { id },
+  payload: { id, buyer_id },
 });
 
-export const removeFromGroupOffer = id => ({
+export const removeOffer = (id, buyer_id) => ({
   type: REMOVE_FROM_GROUP_OFFER,
-  payload: { id },
+  payload: { id, buyer_id },
 });
+
+export const addToGroupOffer = id => (dispatch, getState) => {
+  const buyer_id = getState().currentUser.id;
+  dispatch(addOffer(id, buyer_id));
+};
+
+export const removeFromGroupOffer = id => (dispatch, getState) => {
+  const buyer_id = getState().currentUser.id;
+  dispatch(removeOffer(id, buyer_id));
+};
