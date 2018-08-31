0943a57fb28f019215135ede0f114fc4c559c8cd
Vrezh <hovanisyan.vrezh@gmail.com>
2018-08-31 15:44:15 +0400

    [add] delete data reducer and selector

--- src/reducers/user-data.js
+++ src/reducers/user-data.js
@@ -1,9 +1,11 @@
-import { USER_DATA_SUCCESS } from '../actions/constants';
+import { USER_DATA_SUCCESS, DATA_DELETE_SUCCESS } from '../actions/constants';
 
 const data = (state = [], action) => {
   switch (action.type) {
     case USER_DATA_SUCCESS:
       return action.payload.data;
+    case DATA_DELETE_SUCCESS:
+      return action.payload.data.filter(({ id }) => id !== action.payload.id);
     default:
       return state;
   }
