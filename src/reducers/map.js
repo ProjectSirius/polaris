1300840333d73a72da4b994172300adf7cf5f757
Vrezh <hovanisyan.vrezh@gmail.com>
2018-09-01 10:46:05 +0400

    [fix] bug on map

--- src/reducers/map.js
+++ src/reducers/map.js
@@ -1,18 +1,18 @@
-const SET_ZOOM = 'SET_ZOOM';
+// const SET_ZOOM = 'SET_ZOOM';
 
-const state = {
-  zoom: 11,
-};
+// const state = {
+//   zoom: 11,
+// };
 
-const map = (state = state, action) => {
-  switch (action.type) {
-    case SET_ZOOM:
-      return {
-        ...state,
-        zoom: action.payload.zoom,
-      };
+// const map = (state = state, action) => {
+//   switch (action.type) {
+//     case SET_ZOOM:
+//       return {
+//         ...state,
+//         zoom: action.payload.zoom,
+//       };
 
-    default:
-      break;
-  }
-};
+//     default:
+//       break;
+//   }
+// };
