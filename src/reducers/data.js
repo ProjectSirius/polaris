a130705f27df491eb8151d74cc3770b7fa0942b9
Angela <ang.hayrapetian@gmail.com>
2018-09-05 16:48:46 +0400

    [fix] duplicate ids issue

--- src/reducers/data.js
+++ src/reducers/data.js
@@ -1,11 +1,22 @@
 import { DATA_RECEIVE_SUCCESS } from '../actions/constants';
 
+const uniqueArray = array => {
+  let data = array.concat();
+  for (let i = 0; i < data.length; ++i) {
+    for (let j = i + 1; j < data.length; ++j) {
+      if (data[i].id === data[j].id) data.splice(j--, 1);
+    }
+  }
+
+  return data;
+};
+
 const data = (state = { info: [], totalPages: 0, currentPage: 0 }, action) => {
   switch (action.type) {
     case DATA_RECEIVE_SUCCESS:
       return {
         ...state,
-        info: state.info.concat(action.payload.info),
+        info: uniqueArray(state.info.concat(action.payload.info)),
         totalPages: action.payload.totalPages,
         currentPage: action.payload.currentPage,
       };
