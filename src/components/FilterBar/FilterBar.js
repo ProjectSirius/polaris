acf8bbf56c69cc27a1d8279f1de2b7f36f3eda69
Angela <ang.hayrapetian@gmail.com>
2018-09-05 11:48:07 +0400

    [change] checkboxes filter name

--- src/components/FilterBar/FilterBar.js
+++ src/components/FilterBar/FilterBar.js
@@ -21,7 +21,7 @@ const messages = defineMessages({
   },
   checkboxFilterTitle: {
     id: 'checkboxes-filter-title',
-    defaultMessage: 'Checkboxes',
+    defaultMessage: 'GENRES',
   },
   selectedOptionFilterTitle: {
     id: 'select-option-filter-title',
