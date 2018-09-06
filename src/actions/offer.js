555eade1ed02c9c9648c14ab177548dd57b6f255
Angela <ang.hayrapetian@gmail.com>
2018-09-06 17:56:43 +0400

    [fix] prettier format

--- src/actions/offer.js
+++ src/actions/offer.js
@@ -1,35 +1,32 @@
 import { ACCEPT_OFFER, DECLINE_OFFER } from './constants';
-import {doPost} from '../api/request';
+import { doPost } from '../api/request';
 
-export const accept = (data) => ({
+export const accept = data => ({
   type: ACCEPT_OFFER,
-  payload: data
+  payload: data,
 });
 
-export const acceptOffer = (data) => dispatch => {
-  doPost('', data)
-    .then(data => {
-      if (data.error) {
-        throw new Error(data.error);
-      }
+export const acceptOffer = data => dispatch => {
+  doPost('', data).then(data => {
+    if (data.error) {
+      throw new Error(data.error);
+    }
 
-      return dispatch(accept(data));
-    })
+    return dispatch(accept(data));
+  });
 };
 
-export const decline = (data) => ({
+export const decline = data => ({
   type: DECLINE_OFFER,
-  payload: data
+  payload: data,
 });
 
-export const declineOffer = (data) => dispatch => {
-  doPost('', data)
-    .then(data => {
-      if (data.error) {
-        throw new Error(data.error);
-      }
+export const declineOffer = data => dispatch => {
+  doPost('', data).then(data => {
+    if (data.error) {
+      throw new Error(data.error);
+    }
 
-      return dispatch(decline(data));
-    })
+    return dispatch(decline(data));
+  });
 };
-
